ecfba562e656dcbe2c46909e1dc0e4531a3b9ce2
Devvara Rishivian <rishivian@gmail.com>
2021-10-05 12:21:48 +0700

    feat: create remaining methods

--- pkg/service/v1/todo-service.go
+++ pkg/service/v1/todo-service.go
@@ -134,3 +134,126 @@ func (s *todoServiceServer) Read(ctx context.Context, req *v1.ReadRequest) (*v1.
 		Todo: &td,
 	}, nil
 }
+
+// Update todo task
+func (s *todoServiceServer) Update(ctx context.Context, req *v1.UpdateRequest) (*v1.UpdateResponse, error) {
+	// check if the API version requested by client is supported by server
+	if err := s.checkAPI(req.Api); err != nil {
+		return nil, err
+	}
+
+	// Get SQL connection from pool
+	c, err := s.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+
+	reminder, err := ptypes.Timestamp(req.Todo.Reminder)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, "reminder field has invalid format-> "+err.Error())
+	}
+
+	// Update todo
+	res, err := c.ExecContext(ctx, "UPDATE todo SET title=?, description=?, reminder=? WHERE id=?",
+		req.Todo.Title, req.Todo.Description, reminder, req.Todo.Id)
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed to update Todo-> "+err.Error())
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed to retrieve rows affected value-> "+err.Error())
+	}
+
+	if rows == 0 {
+		return nil, status.Error(codes.NotFound, fmt.Sprintf("Todo with ID='%d' is not found",
+			req.Todo.Id))
+	}
+
+	return &v1.UpdateResponse{
+		Api:     API_VERSION,
+		Updated: rows,
+	}, nil
+}
+
+// Delete todo task
+func (s *todoServiceServer) Delete(ctx context.Context, req *v1.DeleteRequest) (*v1.DeleteResponse, error) {
+	// check if the API version requested by client is supported by server
+	if err := s.checkAPI(req.Api); err != nil {
+		return nil, err
+	}
+
+	// Get SQL connection from pool
+	c, err := s.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+
+	// Delete Todo
+	res, err := c.ExecContext(ctx, "DELETE FROM todo WHERE id=?", req.Id)
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed to delete Todo-> "+err.Error())
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed to retrieve rows affected value-> "+err.Error())
+	}
+
+	if rows == 0 {
+		return nil, status.Error(codes.NotFound, fmt.Sprintf("Todo with ID='%d' is not found",
+			req.Id))
+	}
+
+	return &v1.DeleteResponse{
+		Api:     API_VERSION,
+		Deleted: rows,
+	}, nil
+}
+
+// Read all todo tasks
+func (s *todoServiceServer) ReadAll(ctx context.Context, req *v1.ReadAllRequest) (*v1.ReadAllResponse, error) {
+	// check if the API version requested by client is supported by server
+	if err := s.checkAPI(req.Api); err != nil {
+		return nil, err
+	}
+
+	// Get SQL connection from pool
+	c, err := s.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+
+	// Get Todo list
+	rows, err := c.QueryContext(ctx, "SELECT id, title, description, reminder FROM todo")
+	if err != nil {
+		return nil, status.Error(codes.Unknown, "failed to select from Todo-> "+err.Error())
+	}
+	defer rows.Close()
+
+	var reminder time.Time
+	list := []*v1.Todo{}
+	for rows.Next() {
+		td := new(v1.Todo)
+		if err := rows.Scan(&td.Id, &td.Title, &td.Description, &reminder); err != nil {
+			return nil, status.Error(codes.Unknown, "failed to retrieve field values from Todo row-> "+err.Error())
+		}
+		td.Reminder, err = ptypes.TimestampProto(reminder)
+		if err != nil {
+			return nil, status.Error(codes.Unknown, "reminder field has invalid format-> "+err.Error())
+		}
+		list = append(list, td)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, status.Error(codes.Unknown, "failed to retrieve data from Todo-> "+err.Error())
+	}
+
+	return &v1.ReadAllResponse{
+		Api:   API_VERSION,
+		Todos: list,
+	}, nil
+}
